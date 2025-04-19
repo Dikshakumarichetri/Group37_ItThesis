dc7a18b7d6f5aee1898fb8e3e9de6b16b06a4850
diksha chhetri <dikshachhetri@dikshas-MacBook-Air.local>
2025-04-19 14:20:22 +1000

    adding initial code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Blank 4</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
