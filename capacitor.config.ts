846c92a986a69beec6abe6da3f3927ac64736f0a
diksha chhetri <dikshachhetri@dikshas-MacBook-Air.local>
2025-04-19 14:11:50 +1000

    adding initial code

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.group37.talktrace',
+  appName: 'TalkTrace',
+  webDir: 'build'
+};
+
+export default config;
